075913dfcd393cf1d215dfa5f9918121d870ca8b
Fabio9494 <146652757+Fabio9494@users.noreply.github.com>
2024-03-01 13:18:58 +0100

    Initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable=['type_id','title','date','description','author','img','slug'];
+
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
+
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
+    }
+}
